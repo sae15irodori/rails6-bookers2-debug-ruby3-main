e349ef9ef29f469a5c16e12d83b4b3deaa963861
EC2 Default User <ec2-user@ip-172-31-46-99.ap-northeast-1.compute.internal>
2023-01-26 05:05:08 +0000

    bootstrapでレイアウト

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,5 @@
 class Book < ApplicationRecord
-  belongs_to :user#has_manyより
+  belongs_to :user#has_manyより変更
   validates :title,presence:true
   validates :body,presence:true,length:{maximum:200}
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :books, dependent: :destroy#belongs_toより
+  has_many :books, dependent: :destroy#belongs_toより変更
   has_one_attached :profile_image
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
