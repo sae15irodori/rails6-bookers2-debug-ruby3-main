3c1ec5fb2a80e0567c0680f8912e2c143ca69937
EC2 Default User <ec2-user@ip-172-31-46-99.ap-northeast-1.compute.internal>
2023-01-26 02:28:32 +0000

    Update

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  devise_for :users
-  #root :to =>"homes#top"記述変更
-  root to: "homes#top"
+  root :to =>"homes#top"
   get "home/about"=>"homes#about"
-
+  
+  devise_for :users#最終行より移動
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update]
   resources :users, only: [:index,:show,:edit,:update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  #devise_for :usersこれを一番上へ
+  #devise_for :usersこれをresources :usersの上へ無限ループの原因となる。
 end#追記
