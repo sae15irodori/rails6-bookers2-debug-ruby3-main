5e7d741efca24fceb54508536fff0047a72a6501
EC2 Default User <ec2-user@ip-172-31-46-99.ap-northeast-1.compute.internal>
2023-01-26 07:31:00 +0000

    Update ｱｸｾｽ制限ﾒｿｯﾄﾞ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -49,10 +49,10 @@ class BooksController < ApplicationController
   def book_params
     params.require(:book).permit(:title, :body)#titlemになってた
   end
-  
+
   def ensure_currect_user
     @book = Book.find(params[:id])
-      unless @book == current_user
+      unless @book.user == current_user#@bookの.user(ﾕｰｻﾞｰ情報)がｶﾚﾝﾄﾕｰｻﾞｰじゃなければ
         redirect_to books_path
       end
   end
