cd61f56b1e534a06aa3219aadf8d73c763046f90
EC2 Default User <ec2-user@ip-172-31-46-99.ap-northeast-1.compute.internal>
2023-01-26 02:37:40 +0000

    Add bootstrap fontawesome

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root :to =>"homes#top"
   get "home/about"=>"homes#about"
-  
+
   devise_for :users#最終行より移動
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update]
   resources :users, only: [:index,:show,:edit,:update]
