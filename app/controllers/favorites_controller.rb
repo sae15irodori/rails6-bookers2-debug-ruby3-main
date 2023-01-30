550bc06734a2cebcb5ca0cf960afd608da95f139
EC2 Default User <ec2-user@ip-172-31-46-99.ap-northeast-1.compute.internal>
2023-01-30 01:56:27 +0000

    Add いいね機能

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,16 +1,16 @@
 class FavoritesController < ApplicationController
 
   def create#いいね保存
-    book = Book.find(params[:id])
+    book = Book.find(params[:book_id])
     favorite = current_user.favorites.new(book_id: book.id)
     favorite.save
-    redirect_to book_path(book)
+    redirect_back(fallback_location: books_path)
   end
 
   def destroy
     book = Book.find(params[:book_id])
-    book = current_user.favorites.find_by(book_id: book.id)
-    book.destroy
-    redirect_to book_path(book)
+    favorite = current_user.favorites.find_by(book_id: book.id)
+    favorite.destroy
+    redirect_to request.referer
   end
 end
