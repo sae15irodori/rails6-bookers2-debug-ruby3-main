70407cf7e082b15efe082afdc40e13d7b1fa10da
EC2 Default User <ec2-user@ip-172-31-46-99.ap-northeast-1.compute.internal>
2023-02-01 07:37:20 +0000

    Update フォロー機能

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -6,7 +6,8 @@ class RelationshipsController < ApplicationController
   end
 
   def destroy
-    @user = Relationship.find(params[:id]).followed
+    @user = User.find(params[:user_id])
     current_user.unfollow(@user)
+    redirect_to request.referer
   end
 end
