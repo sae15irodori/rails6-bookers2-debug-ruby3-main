64429cbbbcec84c993dbd3f7bd3c8103fadd1cbf
EC2 Default User <ec2-user@ip-172-31-46-99.ap-northeast-1.compute.internal>
2023-02-01 03:41:15 +0000

    Update

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,3 +1,8 @@
 class Relationship < ApplicationRecord
-  belongs_to :follower, class_name: "User"#
+  belongs_to :follower, class_name: "User"#ﾌｫﾛｰするﾕｰｻﾞのﾃﾞｰﾀをUserモデルから取得できる
+  belongs_to :followed, class_name: "User"#ﾌｫﾛｰされたﾕｰｻﾞｰのﾃﾞｰﾀをUserﾓﾃﾞﾙから取得できる
+  
+  validates :follower_id, presence: true#ﾌｫﾛするﾕｰｻﾞｰのid存在すればtrue
+  validates :followed_id, presence: true#ﾌｫﾛｰされたﾕｰｻﾞｰのid存在すればtrue
+  validates :follower_id, uniqueness: { scope: :followed_id }#ﾌｫﾛﾜｰがﾌｫﾛｰできるのは一人だけ
 end
