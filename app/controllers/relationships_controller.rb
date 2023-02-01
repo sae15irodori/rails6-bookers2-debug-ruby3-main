10b35c48391828db48d19ee75d8d52627177c5f5
EC2 Default User <ec2-user@ip-172-31-46-99.ap-northeast-1.compute.internal>
2023-02-01 06:30:09 +0000

    Add フォロー機能のﾋﾞｭｰﾌｧｲﾙ&Updateフォロー機能コントローラ

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,7 +1,8 @@
 class RelationshipsController < ApplicationController
   def create
-    @user = User.find(params[:followed_id])#フォローされる人のデータを取得
+    @user = User.find(params[:user_id])#フォローされる人のデータを取得
     current_user.follow(@user)#ログイン中ユーザーがフォローする(上の行で取得した人を)
+    redirect_to request.referer
   end
 
   def destroy
