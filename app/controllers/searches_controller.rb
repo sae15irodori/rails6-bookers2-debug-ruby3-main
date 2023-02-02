5cb5089d7569b1761eb03775c112b7e6b5a89095
EC2 Default User <ec2-user@ip-172-31-46-99.ap-northeast-1.compute.internal>
2023-02-02 02:14:05 +0000

    Add 検索機能searchアクション

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,4 +1,12 @@
 class SearchesController < ApplicationController
-  def search
-  end
+   def search
+      @model=params[:model]#userかbook選択される
+      @content=params[:content]#ﾕｰｻﾞｰが入力するﾜｰﾄﾞを受け取る
+      @method=params[:method]#完全､部分､前方､後方一致いずれかが選択される
+      if @model == 'user'#userを選択された場合
+        @records=User.search_for(@content,@method)#Userモデルから探してくる
+      else#そうじゃなければ
+        @records=Book.search_for(@content,@method)#Bookモデルから探してくる
+      end
+    end
 end
