eb79feff690713e6787b7299cd644df68202fe58
EC2 Default User <ec2-user@ip-172-31-46-99.ap-northeast-1.compute.internal>
2023-02-03 08:36:12 +0000

    Add いいね機能を非同期化

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,16 +1,18 @@
 class FavoritesController < ApplicationController
 
   def create#いいね保存
-    book = Book.find(params[:book_id])
-    favorite = current_user.favorites.new(book_id: book.id)
+    @book = Book.find(params[:book_id])
+    favorite = current_user.favorites.new(book_id: @book.id)
     favorite.save
-    redirect_to request.referer
+    #redirect_to request.referer非同期通信の為リダイレクトしない。が、javascriptリクエストとなる。
+    #アクション実行後はcreate.js.erbファイルを探す
   end
 
   def destroy
-    book = Book.find(params[:book_id])
-    favorite = current_user.favorites.find_by(book_id: book.id)
+    @book = Book.find(params[:book_id])
+    favorite = current_user.favorites.find_by(book_id: @book.id)
     favorite.destroy
-    redirect_to request.referer
+    #redirect_to request.referer非同期通信の為リダイレクトしない。が、javascriptリクエストとなる
+    #アクション実行後はdestroy.js.erbファイルを探す
   end
 end
