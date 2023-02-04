51015c8e70c9d4fb62c1ed1bbed18762dfd2e2bf
EC2 Default User <ec2-user@ip-172-31-46-99.ap-northeast-1.compute.internal>
2023-02-04 06:53:44 +0000

    Update

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,11 +1,12 @@
 class BookCommentsController < ApplicationController
 
   def create
-    book = Book.find(params[:book_id])#コメントする投稿データを取得
+    @book = Book.find(params[:book_id])#コメントする投稿データを取得
     comment = current_user.book_comments.new(book_comment_params)#ｶﾚﾝﾄﾕｰｻﾞｰと関連づいてる空のｺﾒﾝﾄﾚｺｰﾄﾞ取得ｺﾒﾝﾄｶﾗﾑのみ保存
-    comment.book_id = book.id#上の行で取得したﾚｺｰﾄﾞの内、book_idｶﾗﾑとコメントする投稿のbook.idを結びつける
+    comment.book_id = @book.id#上の行で取得したﾚｺｰﾄﾞの内、book_idｶﾗﾑとコメントする投稿のbook.idを結びつける
     comment.save
-    #redirect_to request.referer
+    @user = current_user
+    #redirect_to book_path(@book.id)
   end
 
   def destroy
