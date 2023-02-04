feacb02d4ed20749b826461327539d4b0c09eae2
EC2 Default User <ec2-user@ip-172-31-46-99.ap-northeast-1.compute.internal>
2023-02-04 03:04:20 +0000

    Update

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,9 +10,13 @@ import "channels"
 import "jquery"
 import "popper.js"
 import "bootstrap"
-import "../stylesheets/application" 
+import "../stylesheets/application"
 import '@fortawesome/fontawesome-free/js/all';
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+
+//= require jquery
+//= require jquery_ujs
