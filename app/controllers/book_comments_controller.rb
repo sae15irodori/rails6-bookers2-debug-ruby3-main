e0930ce39227f0b7aa7eba36a9d5591637c9a179
EC2 Default User <ec2-user@ip-172-31-46-99.ap-northeast-1.compute.internal>
2023-02-04 01:30:43 +0000

    Add コメント機能非同期通信化

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -5,12 +5,12 @@ class BookCommentsController < ApplicationController
     comment = current_user.book_comments.new(book_comment_params)#ｶﾚﾝﾄﾕｰｻﾞｰと関連づいてる空のｺﾒﾝﾄﾚｺｰﾄﾞ取得ｺﾒﾝﾄｶﾗﾑのみ保存
     comment.book_id = book.id#上の行で取得したﾚｺｰﾄﾞの内、book_idｶﾗﾑとコメントする投稿のbook.idを結びつける
     comment.save
-    redirect_to request.referer
+    #redirect_to request.referer
   end
 
   def destroy
    BookComment.find(params[:id]).destroy
-   redirect_to request.referer
+   #redirect_to request.referer　js.erbを探しに行く
   end
 
   private
