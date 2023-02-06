9b6dc2bcc974bb0a7c96612b02d704e818624786
EC2 Default User <ec2-user@ip-172-31-46-99.ap-northeast-1.compute.internal>
2023-02-06 08:41:58 +0000

    Update 7a

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,12 +10,12 @@ class BooksController < ApplicationController
 
   def index
     #@books = Book.all
-    to  = Time.current.at_end_of_day
-    from  = (to - 6.day).at_beginning_of_day
-    @books = Book.all.sort {|a,b|
-      b.favorites.where(created_at: from...to).size <=>
-      a.favorites.where(created_at: from...to).size
-    }
+    to = Time.current.at_end_of_day
+    from = (to - 6.day).at_beginning_of_day
+    @books = Book.all.sort {|a,b| 
+      a.favorites.where(created_at: from...to).size <=> 
+      b.favorites.where(created_at: from...to).size
+    }.reverse
     @book  = Book.new
     @user = current_user
   end
