23bc2e6ecad100e2cc527d0243ecb6a30f9ae470
EC2 Default User <ec2-user@ip-172-31-46-99.ap-northeast-1.compute.internal>
2023-02-07 09:24:08 +0000

    Addグループ作成機能ビュー&Update groupモデル

--- app/models/group.rb
+++ app/models/group.rb
@@ -4,5 +4,10 @@ class Group < ApplicationRecord
 
   validates :name, presence: true#名前空白ダメ
   validates :introduction, presence: true#自己紹介文空白ダメ
-  attachment :image, destroy: false#画像の削除できない
+  #attachment :image, destroy: false#画像の削除できない
+  has_one_attached :profile_image
+  
+  def get_profile_image
+    (profile_image.attached?) ? profile_image : 'no_image.jpg'
+  end
 end
