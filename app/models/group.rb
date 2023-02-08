28a8ccd0b98e8cd9a7ed361ad8cbd7b77835251b
EC2 Default User <ec2-user@ip-172-31-46-99.ap-northeast-1.compute.internal>
2023-02-08 23:52:38 +0000

    Update

--- app/models/group.rb
+++ app/models/group.rb
@@ -5,9 +5,9 @@ class Group < ApplicationRecord
   validates :name, presence: true#名前空白ダメ
   validates :introduction, presence: true#自己紹介文空白ダメ
   #attachment :image, destroy: false#画像の削除できない
-  has_one_attached :profile_image
+  has_one_attached :image
   
-  def get_profile_image
-    (profile_image.attached?) ? profile_image : 'no_image.jpg'
+  def get_image
+    (image.attached?) ? image : 'no_image.jpg'
   end
 end
