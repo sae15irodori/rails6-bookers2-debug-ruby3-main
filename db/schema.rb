404bd9803cd0d55296b82062a40891315d934639
EC2 Default User <ec2-user@ip-172-31-46-99.ap-northeast-1.compute.internal>
2023-02-18 02:31:54 +0000

    Add タグ機能 book_tag_relationsモデル

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_02_16_023417) do
+ActiveRecord::Schema.define(version: 2023_02_18_023022) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -46,6 +46,15 @@ ActiveRecord::Schema.define(version: 2023_02_16_023417) do
     t.integer "book_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "book_tag_relations", force: :cascade do |t|
+    t.integer "book_id", null: false
+    t.integer "tag_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["book_id"], name: "index_book_tag_relations_on_book_id"
+    t.index ["tag_id"], name: "index_book_tag_relations_on_tag_id"
   end
 
   create_table "books", force: :cascade do |t|
@@ -105,6 +114,12 @@ ActiveRecord::Schema.define(version: 2023_02_16_023417) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "user_rooms", force: :cascade do |t|
     t.integer "user_id"
     t.integer "room_id"
@@ -135,6 +150,8 @@ ActiveRecord::Schema.define(version: 2023_02_16_023417) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "book_tag_relations", "books"
+  add_foreign_key "book_tag_relations", "tags"
   add_foreign_key "group_users", "groups"
   add_foreign_key "group_users", "users"
 end
